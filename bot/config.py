f5d22131a094bae4fb9d9128d189a442e339032c
Akshat009-hash <akshatofficial63@gmail.com>
2026-06-02 17:32:01 +0530

    Update config.py

--- bot/config.py
+++ bot/config.py
@@ -11,7 +11,7 @@ class Config:
 
     SESSION_NAME = ":memory:"
 
-    API_ID = int(os.environ.get("28083979"))
+    API_ID = int(os.environ.get("API_ID"))
 
     API_HASH = os.environ.get("dcf205da67a02806db1a22ed087d9f44")
 
