bbf4fe4c7077cec8884a984f34ae650b8c4dfcc8
Akshat009-hash <akshatofficial63@gmail.com>
2026-06-02 18:17:58 +0530

    Update utubebot.py

--- bot/utubebot.py
+++ bot/utubebot.py
@@ -6,13 +6,13 @@ from .config import Config
 class UtubeBot(Client):
     def __init__(self):
         super().__init__(
-            session_name=Config.SESSION_NAME,
+            name=Config.SESSION_NAME,  # Changed from session_name to name
             bot_token=Config.BOT_TOKEN,
             api_id=Config.API_ID,
             api_hash=Config.API_HASH,
             plugins=dict(root="bot.plugins"),
             workers=6,
-        )
+        )  
         self.DOWNLOAD_WORKERS = 6
         self.counter = 0
         self.download_controller = {}
