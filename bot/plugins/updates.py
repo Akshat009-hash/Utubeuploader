f19ae90141b1f90f09e98c6b68eb33a519b5ba8d
Akshat009-hash <akshatofficial63@gmail.com>
2026-06-02 20:15:19 +0530

    Update updates.py

--- bot/plugins/updates.py
+++ bot/plugins/updates.py
@@ -1,5 +1,6 @@
 from ..config import Config
 from pyrogram import Client, filters
+from pyrogram.enums import ChatAction
 from ..translations import Messages as tr
 
 from pyrogram.types import (
@@ -8,48 +9,103 @@ from pyrogram.types import (
     Message,
     CallbackQuery,
 )
+
 from ..utubebot import UtubeBot
 
-def map(pos):
-    if(pos==1):
-        button = [
-            [InlineKeyboardButton(text = '-->', callback_data = "update+2")]
-        ]
-    elif(pos==len(tr.UPDATE_MSG)-1):
 
+def map(pos):
+    if pos == 1:
         button = [
-        [
-            InlineKeyboardButton(text = 'Support Chat', url = Config.SUPPORT_CHAT_LINK),
-            InlineKeyboardButton(text = 'Feature Request', url = "https://github.com/oVo-HxBots/Utubeitbot/issues/new")
-            ],
-            [InlineKeyboardButton(text = '<--', callback_data = f"update+{pos-1}")]
-          
+            [
+                InlineKeyboardButton(
+                    text='-->',
+                    callback_data="update+2"
+                )
+            ]
         ]
+
+    elif pos == len(tr.UPDATE_MSG) - 1:
+        button = [
+            [
+                InlineKeyboardButton(
+                    text='Support Chat',
+                    url=Config.SUPPORT_CHAT_LINK
+                ),
+                InlineKeyboardButton(
+                    text='Feature Request',
+                    url="https://github.com/oVo-HxBots/Utubeitbot/issues/new"
+                )
+            ],
+            [
+                InlineKeyboardButton(
+                    text='<--',
+                    callback_data=f"update+{pos-1}"
+                )
+            ]
+        ]
+
     else:
         button = [
             [
-                InlineKeyboardButton(text = '<--', callback_data = f"update+{pos-1}"),
-                InlineKeyboardButton(text = '-->', callback_data = f"update+{pos+1}")
-            ],
+                InlineKeyboardButton(
+                    text='<--',
+                    callback_data=f"update+{pos-1}"
+                ),
+                InlineKeyboardButton(
+                    text='-->',
+                    callback_data=f"update+{pos+1}"
+                )
+            ]
         ]
+
     return button
 
-@UtubeBot.on_message(filters.private & filters.incoming & filters.command(['update', 'up']), group=2)
-async def _update(c: UtubeBot, m: Message):
-    await m.reply_chat_action("typing")
+
+@UtubeBot.on_message(
+    filters.private
+    & filters.incoming
+    & filters.command(['update', 'up']),
+    group=2
+)
+async def _update(
+    c: UtubeBot,
+    m: Message
+):
+    await m.reply_chat_action(
+        ChatAction.TYPING
+    )
+
     await m.reply_text(
-        text = tr.UPDATE_MSG[1],
+        text=tr.UPDATE_MSG[1],
         disable_web_page_preview=True,
-        reply_markup = InlineKeyboardMarkup(map(1))
+        reply_markup=InlineKeyboardMarkup(
+            map(1)
+        )
     )
 
 
-update_callback_filter = filters.create(lambda _, __, query: query.data.startswith('update+'))
+update_callback_filter = filters.create(
+    lambda _, __, query:
+    query.data.startswith('update+')
+)
 
-@UtubeBot.on_callback_query(update_callback_filter)
-async def update_answer(c: UtubeBot, q: CallbackQuery):
-    pos = int(q.data.split("+")[1])
+
+@UtubeBot.on_callback_query(
+    update_callback_filter
+)
+async def update_answer(
+    c: UtubeBot,
+    q: CallbackQuery
+):
+    pos = int(
+        q.data.split("+")[1]
+    )
+
     await q.answer()
+
     await q.edit_message_text(
-        text = tr.UPDATE_MSG[pos],    reply_markup = InlineKeyboardMarkup(map(pos))
+        text=tr.UPDATE_MSG[pos],
+        reply_markup=InlineKeyboardMarkup(
+            map(pos)
+        )
     )
