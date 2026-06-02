271e2b87c55848eae481cfe29f5fb26dc0dc1bc9
Akshat009-hash <akshatofficial63@gmail.com>
2026-06-02 20:12:49 +0530

    Update start.py

--- bot/plugins/start.py
+++ bot/plugins/start.py
@@ -1,5 +1,10 @@
 from pyrogram import filters as Filters
-from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton, Message
+from pyrogram.enums import ChatAction
+from pyrogram.types import (
+    InlineKeyboardMarkup,
+    InlineKeyboardButton,
+    Message,
+)
 
 from ..translations import Messages as tr
 from ..config import Config
@@ -12,22 +17,44 @@ from ..utubebot import UtubeBot
     & Filters.command("start")
     & Filters.user(Config.AUTH_USERS)
 )
-async def _start(c: UtubeBot, m: Message):
-    await m.reply_chat_action("typing")
+async def _start(
+    c: UtubeBot,
+    m: Message
+):
+    await m.reply_chat_action(
+        ChatAction.TYPING
+    )
+
     await m.reply_text(
-        text=tr.START_MSG.format(m.from_user.first_name),
+        text=tr.START_MSG.format(
+            m.from_user.first_name
+        ),
         quote=True,
         disable_web_page_preview=True,
-        reply_markup=InlineKeyboardMarkup([
-                  [
-                      InlineKeyboardButton("How To Use Me🙄", callback_data="/help")
-                  ],
-                  [
-                      InlineKeyboardButton("Project Channel!", url="https://t.me/hxbots"),
-                      InlineKeyboardButton("Support Group", url="https://t.me/HxSupport")
-                  ],
-                  [  
-                      InlineKeyboardButton("Upgrade😀", url="https://t.me/+97tA4_TrzyowMjk1")
-                  ]]
+        reply_markup=InlineKeyboardMarkup(
+            [
+                [
+                    InlineKeyboardButton(
+                        "How To Use Me🙄",
+                        callback_data="/help"
+                    )
+                ],
+                [
+                    InlineKeyboardButton(
+                        "Project Channel!",
+                        url="https://t.me/hxbots"
+                    ),
+                    InlineKeyboardButton(
+                        "Support Group",
+                        url="https://t.me/HxSupport"
+                    )
+                ],
+                [
+                    InlineKeyboardButton(
+                        "Upgrade😀",
+                        url="https://t.me/+97tA4_TrzyowMjk1"
+                    )
+                ]
+            ]
         ),
     )
